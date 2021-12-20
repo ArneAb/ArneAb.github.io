bbc068acf59a14ac5dec4cd99c28e6af263ecbc6
ArneAb <96426202+ArneAb@users.noreply.github.com>
2021-12-20 16:47:45 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,64 @@
-const scene = new THREE.scene();
-const camera =new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000); //Might want to change this to ortographic later
-const renderer = new THREE.WebGLRenderer();
+//variables
+let scene, camera, renderer, cube;
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+//Initialize
+function init()
+{
+    scene = new THREE.Scene();
+    camera =new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth  /window.innerHeight,
+        0.1,
+        1000
+    ); //Might want to change this to ortographic later
+    
+    renderer = new THREE.WebGLRenderer({antialias: true});
+    
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    
 
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-camera.position.z = 5;
+    geometry = new THREE.BoxGeometry( 2, 2, 2 );
+    material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    geometry = new THREE.ConeGeometry( 2, 4, 4 );
+    material = new THREE.MeshBasicMaterial( {color: 0x0000ff} );
+    cone = new THREE.Mesh( geometry, material );
+    scene.add( cone );
+    cone.position.x = 10;
+    cube.position.x = -10;
+
+
+    camera.position.z = 20;
+    camera.position.y = 6;
+
+}
+
+
 
 function animate(){
     requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.02;
+
+    cone.rotation.x += 0.01;
+    cone.rotation.y += 0.02;
     renderer.render(scene,camera);
+    const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    scene.add( light );
 }
+
+//When the window gets resized
+function onWindowResize()
+{
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false)
+
+init();
 animate();
